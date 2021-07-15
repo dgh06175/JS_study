70662a34afc6e3cf458d1d525290d78c60c38725
dgh06175 <77305722+dgh06175@users.noreply.github.com>
2021-07-15 17:45:48 +0900

    last

---
+++ js/test.js
@@ -0,0 +1,4 @@
+const myArray = ["hi", "hello", "Lee"];
+myArray.forEach((item)=>console.log(item));
+let func = (a, b) => a+b;
+console.log(func(1,2));
--- js/weather.js
+++ js/weather.js
@@ -2,14 +2,15 @@ const API_KEY = "b30cc6556ff20d51611f568ec182f440";
 function onGeoOk(pos) {
     alert("found you.");
     const lat = pos.coords.latitude;
-    const lon =  pos.coords.longitude;
+    const lon = pos.coords.longitude;
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(response=>response.json()).then(data=>{
+    fetch(url).then(response => response.json()).then(data => {
+        console.dir(document);
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;
         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-    });// JSê°€ urlì„ ì—¶
+    });// JSê°? url?„ ?—¶
 }
 function onGeoError() {
     alert("founding you.");
